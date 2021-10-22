481ee082adfa91e4747c85ef08217196f748a1cb
Tatiane Matuchack <tatianematuchack@yahoo.com.br>
2021-10-22 00:12:39 -0300

    inserida validação somente integer para rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ApplicationRecord
   belongs_to :restaurant
   validates :content, presence: true
   validates :rating, presence: true, inclusion: { in: [0, 1, 2, 3, 4, 5],
-    message: "%{value} is not a valid rating" }
+    message: "%{value} is not a valid rating" }, numericality: { only_integer: true }
 end
